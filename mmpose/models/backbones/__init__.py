5e51c1068def9425adc2601b25924963adc3fa59
Troy Liu <troyliu0105@outlook.com>
2021-10-25 13:28:08 +0800

    A: [Backbone] add RepVGG

--- mmpose/models/backbones/__init__.py
+++ mmpose/models/backbones/__init__.py
@@ -21,11 +21,12 @@ from .shufflenet_v1 import ShuffleNetV1
 from .shufflenet_v2 import ShuffleNetV2
 from .tcn import TCN
 from .vgg import VGG
+from .repvgg import RepVGG
 from .vipnas_resnet import ViPNAS_ResNet
 
 __all__ = [
     'AlexNet', 'HourglassNet', 'HourglassAENet', 'HourglassNetLite', 'HRNet', 'MobileNetV2',
     'MobileNetV3', 'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SCNet',
     'SEResNet', 'SEResNeXt', 'ShuffleNetV1', 'ShuffleNetV2', 'CPM', 'RSN',
-    'MSPN', 'ResNeSt', 'VGG', 'TCN', 'ViPNAS_ResNet', 'LiteHRNet'
+    'MSPN', 'ResNeSt', 'VGG', 'RepVGG', 'TCN', 'ViPNAS_ResNet', 'LiteHRNet'
 ]
