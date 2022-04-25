4f56ae50538aaf6c329b2b6e2a2eca6bd3f3e95f
Troy Liu <troyliu0105@outlook.com>
2022-04-25 22:30:30 +0800

    F: [Config] fix wrong pipeline

--- configs/body/2d_kpt_sview_rgb_img/dekr/stgesture/11pts/hrnet_w16_all.st.beijing_576x384.py
+++ configs/body/2d_kpt_sview_rgb_img/dekr/stgesture/11pts/hrnet_w16_all.st.beijing_576x384.py
@@ -216,14 +216,14 @@ data = dict(
             ann_file=f'{data_root}/rm_shuohuang/9pts.val.json',
             img_prefix=f'{data_root}/rm_shuohuang/9pts_images/',
             data_cfg=data_cfg,
-            pipeline=val_pipeline,
+            pipeline=test_pipeline,
             dataset_info={{_base_.dataset_info}}),
         dict(
             type='BottomUpSTGestureDataset',
             ann_file=f'{data_root}/rm_shuohuang/11pts.val.json',
             img_prefix=f'{data_root}/rm_shuohuang/11pts_images/',
             data_cfg=data_cfg,
-            pipeline=val_pipeline,
+            pipeline=test_pipeline,
             dataset_info={{_base_.dataset_info}}),
     ]
 )
--- configs/body/2d_kpt_sview_rgb_img/dekr/stgesture/11pts/hrnet_w16_coco_576x384.py
+++ configs/body/2d_kpt_sview_rgb_img/dekr/stgesture/11pts/hrnet_w16_coco_576x384.py
@@ -176,13 +176,13 @@ data = dict(
         ann_file=f'{data_root}/annotations/stgesture_person_keypoints_val2017.json',
         img_prefix=f'{data_root}/val2017/',
         data_cfg=data_cfg,
-        pipeline=train_pipeline,
+        pipeline=val_pipeline,
         dataset_info={{_base_.dataset_info}}),
     test=dict(
         type='BottomUpSTGestureDataset',
         ann_file=f'{data_root}/annotations/stgesture_person_keypoints_val2017.json',
         img_prefix=f'{data_root}/val2017/',
         data_cfg=data_cfg,
-        pipeline=train_pipeline,
+        pipeline=test_pipeline,
         dataset_info={{_base_.dataset_info}})
 )
