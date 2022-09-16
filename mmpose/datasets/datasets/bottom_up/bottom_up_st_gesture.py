9840212eccbeefbf808b2136c1eed13bea569ba3
Troy Liu <troyliu0105@outlook.com>
2022-09-16 14:19:28 +0800

    A: [Dataset] filter data_channels

--- mmpose/datasets/datasets/bottom_up/bottom_up_st_gesture.py
+++ mmpose/datasets/datasets/bottom_up/bottom_up_st_gesture.py
@@ -1,5 +1,6 @@
 import json
 
+import numpy as np
 from xtcocotools.cocoeval import COCOeval
 
 from mmpose.datasets.builder import DATASETS
@@ -29,6 +30,53 @@ class BottomUpSTGestureDataset(BottomUpCocoDataset):
                 11: 'left_knee',
                 12: 'left_ankle'
     """
+    def __init__(self, *args, **kwargs):
+        super(BottomUpSTGestureDataset, self).__init__(*args, **kwargs)
+        channels = self.ann_info['dataset_channel'][0]
+        idx_relocate_map = {d: i for (i, d) in enumerate(channels)}
+        self.ann_info['num_joints'] = len(channels)
+        # flip_pairs
+        new_flip_pairs = []
+        for pair in self.ann_info['flip_pairs']:
+            a, b = pair
+            if a in channels and b in channels:
+                new_flip_pairs.append([idx_relocate_map[a], idx_relocate_map[b]])
+        self.ann_info['flip_pairs'] = new_flip_pairs
+        # flip_index
+        flip_index = list(range(len(channels)))
+        for pair in self.ann_info['flip_pairs']:
+            a, b = pair
+            flip_index[a], flip_index[b] = flip_index[b], flip_index[a]
+        self.ann_info['flip_index'] = flip_index
+        # upper_body_ids
+        upper_body_ids = []
+        for i in self.ann_info['upper_body_ids']:
+            if i in channels:
+                upper_body_ids.append(idx_relocate_map[i])
+        self.ann_info['upper_body_ids'] = upper_body_ids
+        # lower_body_ids
+        lower_body_ids = []
+        for i in self.ann_info['lower_body_ids']:
+            if i in channels:
+                lower_body_ids.append(idx_relocate_map[i])
+        self.ann_info['lower_body_ids'] = lower_body_ids
+        # joint_weights
+        self.ann_info['joint_weights'] = self.ann_info['joint_weights'][channels]
+        # skeleton
+        skeleton = []
+        for sk in self.ann_info['skeleton']:
+            a, b = sk
+            if a in channels and b in channels:
+                skeleton.append([idx_relocate_map[a], idx_relocate_map[b]])
+        self.ann_info['skeleton'] = skeleton
+        # coco anns
+        for ann_key in self.coco.anns:
+            ann = self.coco.anns[ann_key]
+            kps = np.array(ann['keypoints'])
+            kps = kps.reshape(-1, 3)[channels]
+            ann['keypoints'] = kps.flatten().tolist()
+            ann['num_keypoints'] = np.count_nonzero(kps[:, -1])
+        self.sigmas = self.sigmas[channels]
 
     def _get_single(self, idx):
         return super()._get_single(idx)
@@ -59,3 +107,8 @@ class BottomUpSTGestureDataset(BottomUpCocoDataset):
         info_str = list(zip(stats_names, coco_eval.stats))
 
         return info_str
+
+    def _get_joints(self, anno):
+        results = super(BottomUpSTGestureDataset, self)._get_joints(anno)
+        # results[:, [0, 7], 2] = (results[:, [0, 7], 2] == 2) * 2
+        return results
