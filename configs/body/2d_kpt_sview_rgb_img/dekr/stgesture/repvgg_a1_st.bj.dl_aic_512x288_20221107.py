4764b2de298ed87599edda37799c2b532733db39
Troy Liu <troyliu0105@outlook.com>
2022-11-07 15:44:33 +0800

    A: [Config] add repvgg_a1_st.bj.dl_aic_512x288_20221107.py

--- configs/body/2d_kpt_sview_rgb_img/dekr/stgesture/repvgg_a1_st.bj.dl_aic_512x288_20221107.py
+++ configs/body/2d_kpt_sview_rgb_img/dekr/stgesture/repvgg_a1_st.bj.dl_aic_512x288_20221107.py
@@ -2,16 +2,81 @@ _base_ = [
     './repvgg_a1_st.bj.dl_aic_512x288_20221026.py',
 ]
 
+channel_cfg = dict(
+    _delete_=True,
+    num_output_channels=13,
+    dataset_joints=13,
+    dataset_channel=[
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    ],
+    inference_channel=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+data_cfg = dict(
+    _delete_=True,
+    image_size=[512, 288],
+    base_size=[256, 144],
+    base_sigma=2,
+    heatmap_size=[[128, 72]],
+    # image_size=512,
+    # base_size=256,
+    # base_sigma=2,
+    # heatmap_size=[128],
+    num_joints=channel_cfg['dataset_joints'],
+    dataset_channel=channel_cfg['dataset_channel'],
+    inference_channel=channel_cfg['inference_channel'],
+    num_scales=1,
+    scale_aware_sigma=False,
+)
+
+model = dict(
+    _delete_=False,
+    type='DisentangledKeypointRegressor',
+    keypoint_head=dict(
+        upsample_use_deconv=True,
+        spp_branch=3,
+        spp_channels=96,
+        num_heatmap_filters=32,
+        use_sigmoid=True,
+        num_offset_filters_per_joint=16,
+        num_offset_filters_layers=1,
+        heatmap_loss=dict(
+            type='JointsMSELoss',
+            use_target_weight=True,
+            supervise_empty=False,
+            loss_weight=1.0,
+        ),
+        offset_loss=dict(
+            type='SoftWeightSmoothL1Loss',
+            use_target_weight=True,
+            supervise_empty=False,
+            loss_weight=0.05,
+            beta=1 / 9.0,
+        )),
+    train_cfg=dict(),
+    test_cfg=dict(
+        num_joints=len(channel_cfg['inference_channel']),
+        max_num_people=30,
+        scale_factor=[1],
+        project2image=False,
+        align_corners=False,
+        max_pool_kernel=3,
+        use_nms=True,
+        nms_dist_thr=0.05,
+        nms_joints_thr=5,
+        keypoint_threshold=0.01,
+        flip_test=False
+    ))
+
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='BottomUpRandomAffine',
-        rot_factor=10,
-        scale_factor=[0.85, 1.15],
+        rot_factor=30,
+        scale_factor=[0.8, 1.2],
         scale_type='long',
-        trans_factor=10,
+        trans_factor=40,
         clip=True),
-    dict(type='BottomUpRandomFlip', flip_prob=0.0),
+    dict(type='BottomUpRandomFlip', flip_prob=0.5),
     dict(
         type='PhotometricDistortion',
         brightness_delta=32,
@@ -20,8 +85,7 @@ train_pipeline = [
         hue_delta=18),
     dict(type='ToTensor'),
     # butt as center
-    dict(type='SelectKeypointAsCenterArea', center_ind=8),
-    # dict(type='GetKeypointCenterArea'),
+    dict(type='GetKeypointCenterArea'),
     dict(
         type='BottomUpGenerateHeatmapTarget',
         sigma=(2, 4),
@@ -38,6 +102,24 @@ train_pipeline = [
         meta_keys=[]),
 ]
 
+val_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='BottomUpGetImgSize', test_scale_factor=[1], base_length=32),
+    dict(type='BottomUpResizeAlign', base_length=32, transforms=[
+        dict(type='ToTensor'),
+    ]),
+    dict(
+        type='Collect',
+        keys=['img'],
+        meta_keys=[
+            'image_file', 'aug_data', 'test_scale_factor', 'base_size',
+            'center', 'scale', 'flip_index', 'num_joints', 'skeleton',
+            'image_size', 'heatmap_size'
+        ]),
+]
+
+test_pipeline = val_pipeline
+
 data_root = 'data'
 data = dict(
     train=[
@@ -46,7 +128,7 @@ data = dict(
             ann_file=
             f'{data_root}/rails/rm_beijing/rm_beijing.20221021.train.json',
             img_prefix=f'{data_root}/rails/rm_beijing/images/',
-            data_cfg={{_base_.data_cfg}},
+            data_cfg=data_cfg,
             pipeline=train_pipeline,
             dataset_info={{_base_.dataset_info}}),
         # dict(
@@ -54,7 +136,7 @@ data = dict(
         #     ann_file=
         #     f'{data_root}/rails/rm_dalian/rm_dalian.20221021.train.json',
         #     img_prefix=f'{data_root}/rails/rm_dalian/images/',
-        #     data_cfg={{_base_.data_cfg}},
+        #     data_cfg=data_cfg,
         #     pipeline=train_pipeline,
         #     dataset_info={{_base_.dataset_info}}),
         dict(
@@ -62,7 +144,7 @@ data = dict(
             ann_file=
             f'{data_root}/rails/rm_shuohuang/rm_shuohuang.11pts.20221021.train.json',
             img_prefix=f'{data_root}/rails/rm_shuohuang/images/',
-            data_cfg={{_base_.data_cfg}},
+            data_cfg=data_cfg,
             pipeline=train_pipeline,
             dataset_info={{_base_.dataset_info}}),
         dict(
@@ -70,7 +152,7 @@ data = dict(
             ann_file=
             f'{data_root}/rails/rm_shuohuang/rm_shuohuang.9pts.20221021.train.json',
             img_prefix=f'{data_root}/rails/rm_shuohuang/images/',
-            data_cfg={{_base_.data_cfg}},
+            data_cfg=data_cfg,
             pipeline=train_pipeline,
             dataset_info={{_base_.dataset_info}}),
         dict(
@@ -78,14 +160,14 @@ data = dict(
             ann_file=
             f'{data_root}/rails/rm_platform/20221025.train.json',
             img_prefix=f'{data_root}/rails/rm_platform/images/',
-            data_cfg={{_base_.data_cfg}},
+            data_cfg=data_cfg,
             pipeline=train_pipeline,
             dataset_info={{_base_.dataset_info}}),
         dict(
             type='BottomUpSTGestureDataset',
             ann_file=f'{data_root}/aic/annotations/st_gesture_aic_train.json',
             img_prefix=f'{data_root}/aic/ai_challenger_keypoint_train_20170909/keypoint_train_images_20170902',
-            data_cfg={{_base_.data_cfg}},
+            data_cfg=data_cfg,
             pipeline=train_pipeline,
             dataset_info={{_base_.dataset_info}})
     ],
@@ -93,53 +175,55 @@ data = dict(
         _delete_=True,
         type='ConcatDataset',
         separate_eval=False,
+        data_cfg=data_cfg,
+        dataset_info={{_base_.dataset_info}},
         datasets=[
             # dict(
             #     type='BottomUpSTGestureDataset',
             #     ann_file=
             #     f'{data_root}/rails/rm_beijing/rm_beijing.20221021.val.json',
             #     img_prefix=f'{data_root}/rails/rm_beijing/images/',
-            #     data_cfg={{_base_.data_cfg}},
-            #     pipeline={{_base_.val_pipeline}},
+            #     data_cfg=data_cfg,
+            #     pipeline=val_pipeline,
             #     dataset_info={{_base_.dataset_info}}),
             # dict(
             #     type='BottomUpSTGestureDataset',
             #     ann_file=
             #     f'{data_root}/rails/rm_dalian/rm_dalian.20221021.val.json',
             #     img_prefix=f'{data_root}/rails/rm_dalian/images/',
-            #     data_cfg={{_base_.data_cfg}},
-            #     pipeline={{_base_.val_pipeline}},
+            #     data_cfg=data_cfg,
+            #     pipeline=val_pipeline,
             #     dataset_info={{_base_.dataset_info}}),
             dict(
                 type='BottomUpSTGestureDataset',
                 ann_file=
                 f'{data_root}/rails/rm_shuohuang/rm_shuohuang.11pts.20221021.val.json',
                 img_prefix=f'{data_root}/rails/rm_shuohuang/images/',
-                data_cfg={{_base_.data_cfg}},
-                pipeline={{_base_.val_pipeline}},
+                data_cfg=data_cfg,
+                pipeline=val_pipeline,
                 dataset_info={{_base_.dataset_info}}),
             # dict(
             #     type='BottomUpSTGestureDataset',
             #     ann_file=
             #     f'{data_root}/rails/rm_shuohuang/rm_shuohuang.9pts.20221021.val.json',
             #     img_prefix=f'{data_root}/rails/rm_shuohuang/images/',
-            #     data_cfg={{_base_.data_cfg}},
-            #     pipeline={{_base_.val_pipeline}},
+            #     data_cfg=data_cfg,
+            #     pipeline=val_pipeline,
             #     dataset_info={{_base_.dataset_info}}),
             dict(
                 type='BottomUpSTGestureDataset',
                 ann_file=
                 f'{data_root}/rails/rm_platform/20221025.val.json',
                 img_prefix=f'{data_root}/rails/rm_platform/images/',
-                data_cfg={{_base_.data_cfg}},
-                pipeline={{_base_.val_pipeline}},
+                data_cfg=data_cfg,
+                pipeline=val_pipeline,
                 dataset_info={{_base_.dataset_info}}),
             # dict(
             #     type='BottomUpSTGestureDataset',
             #     ann_file=f'{data_root}/aic/annotations/st_gesture_aic_val.json',
             #     img_prefix=f'{data_root}/aic/ai_challenger_keypoint_validation_20170911/keypoint_validation_images_20170911',
-            #     data_cfg={{_base_.data_cfg}},
-            #     pipeline={{_base_.val_pipeline}},
+            #     data_cfg=data_cfg,
+            #     pipeline=val_pipeline,
             #     dataset_info={{_base_.dataset_info}}),
         ]
     ),
@@ -147,55 +231,56 @@ data = dict(
         _delete_=True,
         type='ConcatDataset',
         separate_eval=False,
+        data_cfg=data_cfg,
+        dataset_info={{_base_.dataset_info}},
         datasets=[
             # dict(
             #     type='BottomUpSTGestureDataset',
             #     ann_file=
             #     f'{data_root}/rails/rm_beijing/rm_beijing.20221021.val.json',
             #     img_prefix=f'{data_root}/rails/rm_beijing/images/',
-            #     data_cfg={{_base_.data_cfg}},
-            #     pipeline={{_base_.test_pipeline}},
+            #     data_cfg=data_cfg,
+            #     pipeline=test_pipeline,
             #     dataset_info={{_base_.dataset_info}}),
             # dict(
             #     type='BottomUpSTGestureDataset',
             #     ann_file=
             #     f'{data_root}/rails/rm_dalian/rm_dalian.20221021.val.json',
             #     img_prefix=f'{data_root}/rails/rm_dalian/images/',
-            #     data_cfg={{_base_.data_cfg}},
-            #     pipeline={{_base_.test_pipeline}},
+            #     data_cfg=data_cfg,
+            #     pipeline=test_pipeline,
             #     dataset_info={{_base_.dataset_info}}),
             dict(
                 type='BottomUpSTGestureDataset',
                 ann_file=
                 f'{data_root}/rails/rm_shuohuang/rm_shuohuang.11pts.20221021.val.json',
                 img_prefix=f'{data_root}/rails/rm_shuohuang/images/',
-                data_cfg={{_base_.data_cfg}},
-                pipeline={{_base_.test_pipeline}},
+                data_cfg=data_cfg,
+                pipeline=test_pipeline,
                 dataset_info={{_base_.dataset_info}}),
             # dict(
             #     type='BottomUpSTGestureDataset',
             #     ann_file=
             #     f'{data_root}/rails/rm_shuohuang/rm_shuohuang.9pts.20221021.val.json',
             #     img_prefix=f'{data_root}/rails/rm_shuohuang/images/',
-            #     data_cfg={{_base_.data_cfg}},
-            #     pipeline={{_base_.test_pipeline}},
+            #     data_cfg=data_cfg,
+            #     pipeline=test_pipeline,
             #     dataset_info={{_base_.dataset_info}}),
             dict(
                 type='BottomUpSTGestureDataset',
                 ann_file=
                 f'{data_root}/rails/rm_platform/20221025.val.json',
                 img_prefix=f'{data_root}/rails/rm_platform/images/',
-                data_cfg={{_base_.data_cfg}},
-                pipeline={{_base_.test_pipeline}},
+                data_cfg=data_cfg,
+                pipeline=test_pipeline,
                 dataset_info={{_base_.dataset_info}}),
             # dict(
             #     type='BottomUpSTGestureDataset',
             #     ann_file=f'{data_root}/aic/annotations/st_gesture_aic_val.json',
             #     img_prefix=f'{data_root}/aic/ai_challenger_keypoint_validation_20170911/keypoint_validation_images_20170911',
-            #     data_cfg={{_base_.data_cfg}},
-            #     pipeline={{_base_.test_pipeline}},
+            #     data_cfg=data_cfg,
+            #     pipeline=test_pipeline,
             #     dataset_info={{_base_.dataset_info}}),
         ]
     )
 )
-
